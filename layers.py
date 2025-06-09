52e4245e17f4730de16e7ebc9f8a0c546562b6f2
lupi12 <ggeraci.123@gmail.com>
2025-06-09 12:31:06 +0200

    support batches in layers class

--- layers.py
+++ layers.py
@@ -16,8 +16,8 @@ class LinearLayer:
             act_func (callable): Activation function to apply to the layer output.
             act_func_der (callable): Derivative of the activation function.
         """
-        self._in_features = in_features
-        self._out_features = out_features
+        self._in_features = in_features #n_{l-1}
+        self._out_features = out_features #n_l
         self.weights, self.bias = self._init_params()
         self.act_func = act_func
         self.act_func_der = act_func_der
@@ -27,8 +27,8 @@ class LinearLayer:
             "z_l": None
         }
         self.grad_l = None
-        self.grad_w = None
-        self.grad_b = None
+        self.grad_w = np.zeros_like(self.weights)
+        self.grad_b = np.zeros_like(self.bias)
 
         self.weights_m = np.zeros_like(self.weights)
         self.weights_s = np.zeros_like(self.weights)
@@ -58,16 +58,15 @@ class LinearLayer:
             a_l = act_func(z_l)
 
         Args:
-            x (np.ndarray): Input array of shape n_{l-1}x1.
+            x (np.ndarray): Input array of shape (batch_size, n_{l-1}).
 
         Returns:
-            np.ndarray: Output array of shape n_lx1.
+            np.ndarray: Output array of shape (batch_size, n_l).
         """
-        assert (self._in_features, 1) == x.shape, \
+        assert self._in_features == x.shape[1], \
             f"Input is of shape {x.shape} but {self._in_features} input features were expected" # need to add batchsize
-        
         self.cache_dir["x"] = x # a_{l-1}
-        z_l = self.weights@x + self.bias #(out_features,1)
+        z_l = x@self.weights.T + self.bias #(out_features,1)
         self.cache_dir["z_l"] = z_l
         a_l = self.act_func(z_l) #(out_features,1)
         self.cache_dir["a_l"] = a_l
@@ -83,21 +82,23 @@ class LinearLayer:
             grad_b = grad_l
 
         Args:
-            grad_next_layer (np.ndarray): Gradient from the next layer, has shape n_{l+1} x 1
-            weights_next_layer (np.ndarray): Weights of the next layer, has shape n_{l+1}, n_l
+            grad_next_layer (np.ndarray): Gradient from the next layer, has shape (batch_size, n_l+1)
+            weights_next_layer (np.ndarray): Weights of the next layer, has shape (n_{l+1}, n_l)
 
         Returns:
             tuple:
-                - grad_l (np.ndarray): Gradient to propagate to the previous layer, has shape n_l x 1 
-                - grad_w (np.ndarray): Gradient with respect to the layer weights, has shape n_l x n_{l-1}
-                - grad_b (np.ndarray): Gradient with respect to the layer biases, has shape n_l x 1
+                - grad_l (np.ndarray): Gradient to propagate to the previous layer, has shape (batch_size, n_l)
+                - grad_w (np.ndarray): Gradient with respect to the layer weights, has shape (n_l,  n_{l-1})
+                - grad_b (np.ndarray): Gradient with respect to the layer biases, has shape (n_l, 1)
         """
+        z_l = self.cache_dir["z_l"]  # (batch_size, n_l)
+        x = self.cache_dir["x"]      # (batch_size, n_{l-1})
         if weights_next_layer is None:
             grad_l = grad_next_layer # Output Layer
         else:
-            grad_l = weights_next_layer.T @ grad_next_layer * self.act_func_der(self.cache_dir["z_l"])
-        self.grad_w = grad_l@self.cache_dir["x"].T
-        self.grad_b = grad_l
+            grad_l = grad_next_layer @ weights_next_layer * self.act_func_der(z_l)
+        self.grad_w = grad_l.T@x / x.shape[0]
+        self.grad_b = np.sum(grad_l, axis = 0, keepdims=True)/ x.shape[0]
         return grad_l
     
     def update_params(self, t:int, lr: float, beta_1, beta_2, epsilon):
@@ -114,8 +115,9 @@ class LinearLayer:
         bias_s_hat = self.bias_s / (1-beta_2**t)
         
         self.weights -= lr * weights_m_hat/(np.sqrt(weights_s_hat) + epsilon)
+        
         self.bias -= lr * bias_m_hat/(np.sqrt(bias_s_hat) + epsilon)
 
     def zero_grad(self):
-        self.grad_w = None #np.zeros_like(self.weights)
-        self.grad_b = None #np.zeros_like(self.bias)
+        self.grad_w = np.zeros_like(self.weights)
+        self.grad_b = np.zeros_like(self.bias)
